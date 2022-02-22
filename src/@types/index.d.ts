6e14a75b1cd52bbe9f3e1195133306063931a8fc
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-02-21 22:25:59 -0300

    feat: add imagens

---
+++ src/@types/index.d.ts
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-mutable-exports */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module '*.png' {
+  let content: any;
+  export default content;
+}
--- src/screens/teste.tsx
+++ src/screens/teste.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import {Text, View} from 'react-native';
-
-import Icon from 'react-native-vector-icons/Ionicons';
+import {Image, Text, View} from 'react-native';
 
 function Teste() {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>dale daledale</Text>
-      <Icon name="book-outline" size={30} color="#000" />
     </View>
   );
 }
