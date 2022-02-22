eafb5c7db2fc76815858ca9f089268d705ae14d8
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-02-21 21:04:06 -0300

    chore: add themes

---
+++ src/themes/themes.ts
@@ -0,0 +1,13 @@
+export default {
+  color: {
+    primary: {
+      light: '#EFEFEF',
+      dark: '#E5E5E5',
+    },
+    secundary: '#E63A57',
+    blue: {
+      light: '#CAEBF2',
+    },
+    white: '#FFFFFF',
+  },
+};
