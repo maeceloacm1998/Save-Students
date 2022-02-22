3801647dbf7bb01d13f86a0a15b415816eda20be
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-02-22 11:59:23 -0300

    chore: add picket-select

--- src/screens/subjectsList.tsx
+++ src/screens/subjectsList.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View} from 'react-native';
+
+import {Picker} from '@react-native-picker/picker';
 import Header from '../components/header/header';
 
 function SubjectList() {
+  const [selectedLanguage, setSelectedLanguage] = useState();
+
   return (
     <View>
       <Header />
+      <Picker
+        selectedValue={selectedLanguage}
+        onValueChange={(itemValue, itemIndex) =>
+          setSelectedLanguage(itemValue)
+        }>
+        <Picker.Item label="Java" value="java" />
+        <Picker.Item label="JavaScript" value="js" />
+      </Picker>
     </View>
   );
 }
