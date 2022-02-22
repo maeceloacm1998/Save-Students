bacd674a085a0642955e7df186ac36696c1f71ac
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-02-21 21:49:56 -0300

    chore: add vector icons

--- src/screens/teste.tsx
+++ src/screens/teste.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 
+import Icon from 'react-native-vector-icons/Ionicons';
+
 function Teste() {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>dale dale</Text>
+      <Text>dale daledale</Text>
+      <Icon name="book-outline" size={30} color="#000" />
     </View>
   );
 }
