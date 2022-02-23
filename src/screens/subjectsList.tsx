6660d6f46e6c7ab923b66d6206b9a01648e5c5b4
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-02-23 14:43:56 -0300

    feat: selectShift and selectPeriod in home screen

--- src/screens/subjectsList.tsx
+++ src/screens/subjectsList.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import {View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 import Header from '../components/header/header';
 import PickerSelect from '../components/picker-select/picker-select';
+import themes from '../themes/themes';
 
 const array = [
   {
@@ -43,10 +45,55 @@ function SubjectList() {
   const item = (value: string) => {
     console.log(value);
   };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    selectShiftContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      paddingHorizontal: 30,
+    },
+    titleSelectShift: {
+      fontSize: 16,
+      lineHeight: 19,
+      fontFamily: themes.fonts.roboto.regular,
+      color: themes.color.black,
+      marginBottom: 5,
+    },
+  });
+
   return (
-    <View>
+    <View style={styles.container}>
       <Header />
-      <PickerSelect listPickerItem={array} itemSelected={item} />
+
+      <View style={styles.selectShiftContainer}>
+        <View>
+          <Text style={styles.titleSelectShift}>Período</Text>
+          <PickerSelect
+            listPickerItem={array}
+            itemSelected={item}
+            customStyle={{
+              width: 150,
+              height: 37,
+              backgroundColor: themes.color.blue.light,
+            }}
+          />
+        </View>
+        <View>
+          <Text style={styles.titleSelectShift}>Turno</Text>
+          <PickerSelect
+            listPickerItem={array}
+            itemSelected={item}
+            customStyle={{
+              width: 150,
+              height: 37,
+              backgroundColor: themes.color.blue.light,
+            }}
+          />
+        </View>
+      </View>
     </View>
   );
 }
--- src/themes/themes.ts
+++ src/themes/themes.ts
@@ -8,6 +8,7 @@ export default {
     blue: {
       light: '#CAEBF2',
     },
+    black: "#000000",
     white: '#FFFFFF',
   },
   fonts: {
