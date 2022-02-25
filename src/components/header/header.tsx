adb2c963f933b1260f4a3627cb3bf09d278747aa
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-02-25 17:58:20 -0300

    refactor: create types header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,41 @@
+/* eslint-disable react/require-default-props */
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable react/no-unused-prop-types */
 import React from 'react';
-import {StyleSheet, Text, View, Image, Pressable} from 'react-native';
-import {RFPercentage, RFValue} from 'react-native-responsive-fontsize';
+import {StyleSheet, View, Image, Pressable} from 'react-native';
+import {RFValue} from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.png';
 import themes from '../../themes/themes';
 import Icon from '../icon/icon';
 
-function Header() {
+type HeaderType = {
+  showBackButton?: boolean;
+  onPressMenu: () => void;
+  onPressBackButton?: () => void;
+  onPressHelp: () => void;
+  onPressLogo: () => void;
+};
+
+const translate = (props: HeaderType) => ({
+  showBackButton: props.showBackButton ? props.showBackButton : false,
+  onPressMenu: props.onPressMenu ? props.onPressMenu : () => {},
+  onPressBackButton: props.onPressBackButton
+    ? props.onPressBackButton
+    : () => {},
+  onPressHelp: props.onPressHelp ? props.onPressHelp : () => {},
+  onPressLogo: props.onPressLogo ? props.onPressLogo : () => {},
+});
+
+function Header(props: HeaderType) {
+  const {
+    onPressBackButton,
+    onPressHelp,
+    onPressLogo,
+    onPressMenu,
+    showBackButton,
+  } = translate(props);
+
   const styles = StyleSheet.create({
     container: {
       height: RFValue(96),
@@ -24,18 +53,29 @@ function Header() {
 
   return (
     <View style={styles.container}>
-      <Pressable>
-        <Icon
-          type="Ionicons"
-          name="ios-menu"
-          size={30}
-          color={themes.color.secundary}
-        />
-      </Pressable>
-      <Pressable>
+      {showBackButton ? (
+        <Pressable onPressIn={onPressBackButton}>
+          <Icon
+            type="Ionicons"
+            name="chevron-back"
+            size={30}
+            color={themes.color.secundary}
+          />
+        </Pressable>
+      ) : (
+        <Pressable onPressIn={onPressMenu}>
+          <Icon
+            type="Ionicons"
+            name="ios-menu"
+            size={30}
+            color={themes.color.secundary}
+          />
+        </Pressable>
+      )}
+      <Pressable onPressIn={onPressLogo}>
         <Image source={Logo} style={styles.logo} />
       </Pressable>
-      <Pressable>
+      <Pressable onPressIn={onPressHelp}>
         <Icon
           type="Ionicons"
           name="ios-help-circle-outline"
