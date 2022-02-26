f3804ceb38cca642a6195cec0bd6ad181f994af4
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-02-26 02:44:30 -0300

    feat: add calendar and function paintSelectedDate

--- src/screens/scheduleList.tsx
+++ src/screens/scheduleList.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+/* eslint-disable no-shadow */
+import React, {useState} from 'react';
+import {View} from 'react-native';
+
+import {Calendar} from 'react-native-calendars';
+import {MarkingProps} from 'react-native-calendars/src/calendar/day/marking';
 
 import Header from '../components/header/header';
 
 import {ScreenProps} from '../utils/types';
 
+type MarkedDatesType = {
+  [key: string]: MarkingProps;
+};
+
 function ScheduleList({navigation}: ScreenProps) {
   const backButton = () => navigation.navigate('Materias');
   const logo = () => navigation.navigate('Materias');
+
+  const [selectedDates, setSelectedDates] = useState<MarkedDatesType>({});
+
+  const paintSelectedDate = (date: string): void => {
+    const markedDates = {};
+    Object.defineProperty(markedDates, date, {
+      value: {color: '#70d7c7', textColor: 'white'},
+      writable: true,
+      enumerable: true,
+      configurable: true,
+    });
+
+    setSelectedDates({...markedDates, ...selectedDates});
+  };
+
+  const selectDay = (date: string): void => {
+    paintSelectedDate(date);
+  };
 
   return (
     <View>
@@ -15,6 +41,14 @@ function ScheduleList({navigation}: ScreenProps) {
         showBackButton
         onPressBackButton={backButton}
         onPressLogo={logo}
+      />
+
+      <Calendar
+        markingType="period"
+        markedDates={selectedDates}
+        onDayPress={dia => {
+          selectDay(dia.dateString);
+        }}
       />
     </View>
   );
