724ed2ed5d53e4965103ddeed6d4e522101014a6
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-06-12 19:14:57 -0300

    feat: add MaterialIcon type in icon component

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -1,23 +1,25 @@
 /* eslint-disable react/no-unused-prop-types */
-/* eslint-disable consistent-return */
+/* eslint-disable react/require-default-props */
 /* eslint-disable default-case */
+
 import React from 'react';
 
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Feather from 'react-native-vector-icons/Feather';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import themes from '../../themes/themes';
 
 type Icon = {
   name: string;
-  color: string;
+  color?: string;
   size: number;
-  type: 'AntDesign' | 'Ionicons' | 'Feather';
+  type: 'AntDesign' | 'Ionicons' | 'Feather' | 'MaterialIcons';
 };
 
 const translate = (props: Icon) => ({
   name: props.name ? props.name : 'ios-help-circle-outline',
-  color: props.color ? props.color : themes.color.primary.dark,
+  color: props.color ? props.color : themes.color.soft_black,
   size: props.size ? props.size : 20,
   type: props.type ? props.type : 'Ionicons',
 });
@@ -32,6 +34,8 @@ function Icon(props: Icon) {
       return <Ionicons name={name} size={size} color={color} />;
     case 'Feather':
       return <Feather name={name} size={size} color={color} />;
+    case 'MaterialIcons':
+      return <MaterialIcons name={name} size={size} color={color} />;
   }
 }
 
